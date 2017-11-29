13b81327abf1e0b789c0a81cc5c966bf71687e14
npolyak <nick.polyak@verizon.net>
2017-11-28 19:59:53 -0500

    added PlainTypeVariables.ts

---
+++ Variables/Variables/Scripts/PlainTypeVariables.ts
@@ -0,0 +1,26 @@
+﻿let myNumber: number;
+
+let myString: string;
+
+let myAny1: any = null;
+
+// no type specified === any
+let myAny2; 
+
+// initialised
+let myBool1 = true;
+
+// type specified and initialization
+let myBool2: boolean = false;
+
+// compiler error
+// cannot reassign the type to 'number'
+myString = 5; 
+
+myAny1 = 5; //OK
+
+myAny2 == "hello world";
+
+// any can be reassigned any type (like object in C#)
+myAny1 = "Hi";
+
