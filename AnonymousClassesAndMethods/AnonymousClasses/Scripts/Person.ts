2d4b88a09d2a7b304b2de5a639cc18a29cc5fffa
npolyak <nick.polyak@verizon.net>
2017-11-29 18:20:29 -0500

    checkpoint

--- AnonymousClassesAndMethods/AnonymousClasses/Scripts/Person.ts
+++ AnonymousClassesAndMethods/AnonymousClasses/Scripts/Person.ts
@@ -1,23 +1,4 @@
-﻿
-
-let PrintFullName = (person: IPerson) =>
- {
-    let result: string = "";
-
-    if (person.FirstName)
-    {
-        result += person.FirstName + " ";
-    }
-
-    if (person.LastName)
-    {
-        result += person.LastName;
-    }
-
-    alert(result);
-}
-
-// create anonymous class
+﻿// create anonymous class
 let person = new class implements IPerson
 {
     private _firstName: string;
@@ -47,5 +28,23 @@ let person = new class implements IPerson
         this.LastName = lastName;
     }
 }("Joe", "Doe"); // call constructor
+
+
+let PrintFullName = (person: IPerson) =>
+{
+    let result: string = "";
+
+    if (person.FirstName)
+    {
+        result += person.FirstName + " ";
+    }
+
+    if (person.LastName)
+    {
+        result += person.LastName;
+    }
+
+    alert(result);
+}
 
 PrintFullName(person);
