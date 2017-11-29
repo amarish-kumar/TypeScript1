70a04affb31f5778f2ba2e123f5e23cf3f240a2c
npolyak <nick.polyak@verizon.net>
2017-11-29 13:55:12 -0500

    created ExtendingClass project

---
+++ ExtendingClass/ExtendingClass/Scripts/Employee.ts
@@ -0,0 +1,31 @@
+﻿class Employee extends Person
+{
+    private _employeeNumber: number;
+    get EmployeeNumber()
+    {
+        return this._employeeNumber;
+    }
+
+    // property overriding sample
+    get LastName() 
+    {
+        return "SuperCaleFlageolisticExpialodocios";
+    }
+
+    constructor(employeeNumber:number, firstName:string, lastName:string)
+    {
+        // call constructor of the super class
+        super(firstName, firstName);
+
+        this._employeeNumber = employeeNumber;
+    }
+}
+
+function PrintPersonFullName(person: Person): void
+{
+    alert(person.FullName);
+}
+
+let joeDoeEmployee: Person = new Employee(1, "Joe", "Doe");
+
+PrintPersonFullName(joeDoeEmployee);
