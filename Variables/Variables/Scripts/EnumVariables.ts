c10884edfbcd960ba7b67597dd8abac2b899f83f
npolyak <nick.polyak@verizon.net>
2017-11-28 20:17:51 -0500

    checkpoint

---
+++ Variables/Variables/Scripts/EnumVariables.ts
@@ -0,0 +1,17 @@
+﻿export enum Season
+{
+    Winter,
+    Spring = 1, // can assign integer 
+    Summer,
+    Fall
+}
+
+let summerNumber: number;
+
+// summerNumber = 2
+summerNumber = Season.Summer; 
+
+let summerName: string;
+
+// summerName = "Summer"
+summerName = Season[Season.Summer];
